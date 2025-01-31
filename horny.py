d962939539f980e11fa3e34cac847bcd4d7d8e9e
UwUyourmom <louis02102005@gmail.com>
2025-01-31 11:05:11 +0100

    ngs

--- horny.py
+++ horny.py
@@ -3,7 +3,8 @@ from time import sleep
 from random import randint
 i = 0
 username = input("Enter username:")
-Shit = [f'U are so gay {username} !', f'u are a faggot {username}', f' U are a bitch {username}', f'Fuck u {username}', f'Kill urself {username}']
+Shit = [f'U are so gay {username} !', f'u are a faggot {username}', f' U are a bitch {username}', f'Fuck u {username}', f'Kill urself {username}', f'U are a victim of an unfortunate turnover {username}',
+        f'U are a pussy ass, {username}',f'U Motherfucker, {username}', f'cocksucker',f'dumb cunt', 'cocksucker', 'dickhead', 'prick', 'fuckwad', f'U are a lunatic, {username}', 'U sheepfucker']
 repeat = int(input('How often should i tell u?(please only int :3):'))
 while i < repeat:
         print(Shit[randint(0,len(Shit)-1)])
@@ -11,3 +12,13 @@ while i < repeat:
         print(i)
         sleep(0.5)
 print('u gay fuck')
+
+
+
+
+#v, , fucker, motherfucker, pussy,
+#pussy-ass, retard, imbecile, cunt, cunt-licker, dumb cunt, cunt-eyed, pussy-ass,
+#cunt-fucker, frightfucker, cocksucker, dickhead, prick, dickwad, fuckwad, fuckwad,
+#sheepfucker, cocksucker, arsehole, arsehole, arsehole, arsehole, fuckwad, scumbag,
+#dickhead, cocksucker, lunatic, cunt-eared cunt-tail, cunt-fucker, cunt-fucker, cunt-fucker,
+#cunt-head, cunt-fucking cunt, cunt with ears, cunt-fucker, cunt-fucker, cunt-fucker.
