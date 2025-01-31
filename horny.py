e2e5484e471a6c78e24c1fd16b8a91a36622d59d
UwUyourmom <louis02102005@gmail.com>
2025-01-31 12:29:07 +0100

    ngs

--- horny.py
+++ horny.py
@@ -1,15 +1,13 @@
 import time
 from time import sleep
 from random import randint
-i = 0
+
 username = input("Enter username:")
 Shit = [f'U are so gay {username} !', f'u are a faggot {username}', f' U are a bitch {username}', f'Fuck u {username}', f'Kill urself {username}', f'U are a victim of an unfortunate turnover {username}',
         f'U are a pussy ass, {username}',f'U Motherfucker, {username}', f'cocksucker',f'dumb cunt', 'cocksucker', 'dickhead', 'prick', 'fuckwad', f'U are a lunatic, {username}', 'U sheepfucker']
 repeat = int(input('How often should i tell u?(please only int :3):'))
-while i < repeat:
+for i in range(repeat):
         print(Shit[randint(0,len(Shit)-1)])
-        i = i + 1
-        print(i)
         sleep(0.5)
 print('u gay fuck')
 
