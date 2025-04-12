f624813ae96aaf5468579c22024d491d6839e2c4
UwUyourmom <louis02102005@gmail.com>
2025-04-12 17:15:45 +0200

    add stuff

--- horny.py
+++ horny.py
@@ -1,22 +1,35 @@
-import time
 from time import sleep
 from random import randint
 
 username = input("Enter username:")
-Shit = [f'U are so gay {username} !', f'u are a faggot {username}', f' U are a bitch {username}', f'Fuck u {username}', f'Kill urself {username}', f'U are a victim of an unfortunate turnover {username}',
-        f'U are a pussy ass, {username}',f'U Motherfucker, {username}', f'cocksucker',f'dumb cunt', 'cocksucker', 'dickhead', 'prick', 'fuckwad', f'U are a lunatic, {username}', 'U sheepfucker']
-repeat = int(input('How often should i tell u?(please only int :3):'))
+Shit = [
+    f"U are so gay {username} !",
+    f"u are a faggot {username}",
+    f" U are a bitch {username}",
+    f"Fuck u {username}",
+    f"Kill urself {username}",
+    f"U are a victim of an unfortunate turnover {username}",
+    f"U are a pussy ass, {username}",
+    f"U Motherfucker, {username}",
+    "cocksucker",
+    "dumb cunt",
+    "cocksucker",
+    "dickhead",
+    "prick",
+    "fuckwad",
+    f"U are a lunatic, {username}",
+    "U sheepfucker",
+]
+repeat = int(input("How often should i tell u?(please only int :3):"))
 for i in range(repeat):
-        print(Shit[randint(0,len(Shit)-1)])
-        sleep(0.5)
-print('u gay fuck')
+    print(Shit[randint(0, len(Shit) - 1)])
+    sleep(0.5)
+print("u gay fuck")
 
 
-
-
-#v, , fucker, motherfucker, pussy,
-#pussy-ass, retard, imbecile, cunt, cunt-licker, dumb cunt, cunt-eyed, pussy-ass,
-#cunt-fucker, frightfucker, cocksucker, dickhead, prick, dickwad, fuckwad, fuckwad,
-#sheepfucker, cocksucker, arsehole, arsehole, arsehole, arsehole, fuckwad, scumbag,
-#dickhead, cocksucker, lunatic, cunt-eared cunt-tail, cunt-fucker, cunt-fucker, cunt-fucker,
-#cunt-head, cunt-fucking cunt, cunt with ears, cunt-fucker, cunt-fucker, cunt-fucker.
+# v, , fucker, motherfucker, pussy,
+# pussy-ass, retard, imbecile, cunt, cunt-licker, dumb cunt, cunt-eyed, pussy-ass,
+# cunt-fucker, frightfucker, cocksucker, dickhead, prick, dickwad, fuckwad, fuckwad,
+# sheepfucker, cocksucker, arsehole, arsehole, arsehole, arsehole, fuckwad, scumbag,
+# dickhead, cocksucker, lunatic, cunt-eared cunt-tail, cunt-fucker, cunt-fucker, cunt-fucker,
+# cunt-head, cunt-fucking cunt, cunt with ears, cunt-fucker, cunt-fucker, cunt-fucker.
